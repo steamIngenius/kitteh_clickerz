ed5c9ddf2670d216c08a4f4a99e9285dbeb789e0
Micah Hawkins <micahbhawkins@gmail.com>
2015-03-02 22:59:46 -0800

    2 kittehs working properly - code is no longer DRY and needs refactoring

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
-var score = 0;
-var scoreBoard = $('#scoreBoard');
+var score1 = 0;
+var score2 = 0;
 
-$('#kitteh').click(function () {
+$('#kitteh1').click(function () {
 	console.log('The kitteh was clicked!');
-	score++;
-	scoreBoard.text('Score: ' + score);
-	$('#kitteh').effect("shake");
+	score1++;
+	$('#scoreBoard1').text('Score: ' + score1);
+	$('#kitteh1').effect("shake");
+});
+
+$('#kitteh2').click( function () {
+	console.log("Another kitteh was clicked!!");
+	score2++;
+	$('#scoreBoard2').text('Score: ' + score2);
+	$('#kitteh2').effect("shake");
 });
