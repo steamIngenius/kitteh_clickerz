59557e690c886d0d6909e73fd5cdfcce9b168477
Micah Hawkins <micahbhawkins@gmail.com>
2015-03-02 23:47:28 -0800

    Meow!!

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 var score1 = 0;
 var score2 = 0;
 
+$('<audio id="meow1" src="lib/audio/1.mp3" type="audio/mpeg"></audio>').appendTo('body');
+$('<audio id="meow2" src="lib/audio/2.mp3" type="audio/mpeg"></audio>').appendTo('body');
+$('<audio id="meow3" src="lib/audio/3.mp3" type="audio/mpeg"></audio>').appendTo('body');
+$('<audio id="meow4" src="lib/audio/4.mp3" type="audio/mpeg"></audio>').appendTo('body');
+
 $('#kitteh1').click(function () {
 	console.log('The kitteh was clicked!');
 	score1++;
 	$('#scoreBoard1').text('Score: ' + score1);
 	$('#kitteh1').effect("shake");
+	var sound = Math.floor(Math.random()*4)+1;
+	$('#meow'+sound)[0].play();
 });
 
 $('#kitteh2').click( function () {
@@ -13,4 +20,6 @@ $('#kitteh2').click( function () {
 	score2++;
 	$('#scoreBoard2').text('Score: ' + score2);
 	$('#kitteh2').effect("shake");
+	var sound = Math.floor(Math.random()*4)+1;
+	$('#meow'+sound)[0].play();
 });
