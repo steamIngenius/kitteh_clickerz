f6d4d425b83fb3cb0d62ebd67d84f324246da501
Micah Hawkins <mhawkins@extensis.com>
2015-03-03 14:41:36 -0800

    Rough working prototype for Kitteh Clickerz Premium

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-var score1 = 0;
-var score2 = 0;
+var Kitteh = function(name) {
+	this.score = 0;
+	this.name = name;
+};
 
 // add our meows to the page by inserting html
 $('<audio id="meow1" src="lib/audio/1.mp3" type="audio/mpeg"></audio>').appendTo('body');
@@ -7,24 +9,29 @@ $('<audio id="meow2" src="lib/audio/2.mp3" type="audio/mpeg"></audio>').appendTo
 $('<audio id="meow3" src="lib/audio/3.mp3" type="audio/mpeg"></audio>').appendTo('body');
 $('<audio id="meow4" src="lib/audio/4.mp3" type="audio/mpeg"></audio>').appendTo('body');
 
-$('#kitteh1').click(function () {
+var kittehs = [];
+kittehs.push(new Kitteh("Paul"));
+kittehs.push(new Kitteh("John"));
+kittehs.push(new Kitteh("George"));
+kittehs.push(new Kitteh("Ringo"));
+
+
+$('#kitteh').click(function () {
 	// log this event
-	console.log('The kitteh was clicked!');
+	var currentKitteh = $('#currentKitteh').val();
+	var kitteh = kittehs[currentKitteh];
+	console.log(kittehs[currentKitteh]);
 	// score!
-	score1++;
-	$('#scoreBoard1').text('Score: ' + score1);
+	kitteh.score++;
+	$('#scoreboard').text('Score: ' + kitteh.score);
 	// visual feedback
-	$('#kitteh1').effect("shake");
+	$('#kitteh').effect("shake");
 	// audio feedback
 	var sound = Math.floor(Math.random()*4)+1; // random cat sound
 	$('#meow'+sound)[0].play();
 });
 
-$('#kitteh2').click( function () {
-	console.log("Another kitteh was clicked!!");
-	score2++;
-	$('#scoreBoard2').text('Score: ' + score2);
-	$('#kitteh2').effect("shake");
-	var sound = Math.floor(Math.random()*4)+1;
-	$('#meow'+sound)[0].play();
+$('#currentKitteh').change( function() {
+	var currentKitteh = $('#currentKitteh').val();
+	$('#scoreboard').text('Score: ' + kittehs[currentKitteh].score)
 });
